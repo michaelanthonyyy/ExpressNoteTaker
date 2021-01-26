ec95dbc5c06930759e2ed83b92669b3b7faf6b87
Michael Medina <michaelanthony.medinaa@gmail.com>
2021-01-25 19:57:35 -0800

    added app.post utilizing db.json directory file for notes to save to. ID's given to each note saved

--- server.js
+++ server.js
@@ -5,9 +5,8 @@ var fs = require("fs");
 var app = express();
 var PORT = 3000;
 
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 
 
 // HTML Routes (frontend)
@@ -20,6 +19,7 @@ app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "public/notes.html"));
 });
 
+
 // API Routes (backend)
 // =============================================================
 // Changed from return response to db.json after checking instructions
@@ -29,14 +29,19 @@ app.get("/api/notes", function (req, res) {
 
 // Receive new notes to save on the request body and return the new note to the client
 app.post("/api/notes"), function (req, res) {
-    let newNote = req.body;
-    console.log(noteId);
+    let notes = JSON.parse(fs.readFileSync("/db/db.json", "utf-8"))
+    let newNotes = req.body;
+    let notesID = (notes.length).toString();
+    newNotes.id = notesID;
+    notes.push(newNotes);
+    fs.writeFileSync("/db/db.json", JSON.stringify(notes)); {
+        if (err) throw err;
+    }
 }
 
 // Delete notes by unique ID
 app.delete("/api/notes/:id", function (req, res) {
 })
-
 
 
 // Starts the server to begin listening
