a95b4611ac37fed94bec445407e85509ff36e121
Michael Medina <michaelanthony.medinaa@gmail.com>
2021-01-25 19:29:09 -0800

    changed app.get to redirect to db/db.json to json notes with title and text

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var path = require("path");
+var fs = require("fs");
 
 var app = express();
 var PORT = 3000;
@@ -8,37 +9,38 @@ app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
 
+
 // HTML Routes (frontend)
 // =============================================================
 app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "assets/index.html"));
+    res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
 app.get("/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "assets/notes.html"));
+    res.sendFile(path.join(__dirname, "public/notes.html"));
 });
 
-
-// Reads and displays all saved notes as JSON
+// API Routes (backend)
+// =============================================================
+// Changed from return response to db.json after checking instructions
 app.get("/api/notes", function (req, res) {
-    return res.json(notes);
+    res.sendFile(path.join(__dirname, "db/db.json"));
 });
 
 // Receive new notes to save on the request body and return the new note to the client
 app.post("/api/notes"), function (req, res) {
-    var newNote = req.body;
-    console.log(newNote);
+    let newNote = req.body;
+    console.log(noteId);
 }
 
 // Delete notes by unique ID
 app.delete("/api/notes/:id", function (req, res) {
-    var index = req.body.index;
-    var temp = [];
-    for (var i = 0; i < notes.length; i++) {
-        if (i !== parseInt(index)) {
-            temp.push(tables[i]);
-        }
-    }
-    tables = temp;
-    res.send("note removed!")
 })
+
+
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
