af799e5a42126363b884de32b28f0f693002b356
Michael Medina <michaelanthony.medinaa@gmail.com>
2021-01-25 18:48:25 -0800

    API routes created for adding and deleting

--- server.js
+++ server.js
@@ -11,11 +11,34 @@ app.use(express.json());
 // HTML Routes (frontend)
 // =============================================================
 app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
+    res.sendFile(path.join(__dirname, "assets/index.html"));
 });
 
 app.get("/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "notes.html"));
+    res.sendFile(path.join(__dirname, "assets/notes.html"));
 });
 
 
+// Reads and displays all saved notes as JSON
+app.get("/api/notes", function (req, res) {
+    return res.json(notes);
+});
+
+// Receive new notes to save on the request body and return the new note to the client
+app.post("/api/notes"), function (req, res) {
+    var newNote = req.body;
+    console.log(newNote);
+}
+
+// Delete notes by unique ID
+app.delete("/api/notes/:id", function (req, res) {
+    var index = req.body.index;
+    var temp = [];
+    for (var i = 0; i < notes.length; i++) {
+        if (i !== parseInt(index)) {
+            temp.push(tables[i]);
+        }
+    }
+    tables = temp;
+    res.send("note removed!")
+})
