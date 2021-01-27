1873abbc90555ed2d235cc7a3e2c0e350e3575b6
Michael Medina <michaelanthony.medinaa@gmail.com>
2021-01-26 22:42:16 -0800

    changed port to production syntax from heroku documentation

--- server.js
+++ server.js
@@ -3,16 +3,12 @@ var path = require("path");
 var fs = require("fs");
 
 var app = express();
-
+var PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'))
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Our app is running on port ${ PORT }`);
-});
 
 // HTML Routes (frontend)
 // =============================================================
