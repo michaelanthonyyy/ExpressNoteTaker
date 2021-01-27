849f0b8750ef56feda9c07055cc3b87901dbe7ad
Michael Medina <michaelanthony.medinaa@gmail.com>
2021-01-26 20:18:07 -0800

    got db.json to parse correctly to be able to add new notes onto page

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'))
 
-let notes = []
 // HTML Routes (frontend)
 // =============================================================
 app.get("/", function (req, res) {
@@ -30,15 +29,15 @@ app.get("/api/notes", function (req, res) {
 
 // reads db.json file assigning id to each note
 app.post("/api/notes", function (req, res) {
-    fs.readFile(path.join(__dirname, "./db/db.json"), "utf8", function (err) {
-        if (err) throw err;
-    });
-    let noteBody = req.body;
-        // correctly writes file to db.json
-        fs.writeFile("./db/db.json", JSON.stringify(noteBody), function (err) {
-                if (err) console.log(noteBody);
-        });
-});
+    let notesObject = JSON.parse(fs.readFileSync('./db/db.json'));
+    let noteBody = req.body
+    notesObject.push(noteBody);
+    // writes file to db.json
+    fs.writeFile("./db/db.json", JSON.stringify(notesObject), function (err) {
+        if (err) console.log(notes);
+    })
+    res.send();
+})
 
 
 
@@ -46,6 +45,7 @@ app.post("/api/notes", function (req, res) {
 app.delete('/api/notes/:id', function (req, res) {
 
 });
+
 
 // Starts the server to begin listening
 // =============================================================
